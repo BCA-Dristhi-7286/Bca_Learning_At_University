0cbce2d259daf74950adb27d26e41f263c2469fc
BCA-Dristhi-7286 <127825872+BCA-Dristhi-7286@users.noreply.github.com>
2023-04-13 08:31:25 -0700

    Update Assignment5.c

--- Assignment5.c
+++ Assignment5.c
@@ -4,5 +4,35 @@
 
 int main()
 {
+  #define MAX_STUDENTS 50
+#define MIN_PASS_MARKS 50
+
+int main()
+{
+    int marks[MAX_STUDENTS];
+    int n, i, maxMarks = -1, passCount = 0, failCount = 0;
+    
+   printf("Enter the marks of %d students:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &marks[i]);
+        if (marks[i] > maxMarks) {
+            maxMarks = marks[i];
+        }
+        if (marks[i] == -1) {
+            printf("Student %d was absent\n", i+1);
+        }
+        else if (marks[i] >= MIN_PASS_MARKS) {
+            printf("Student %d passed the exam\n", i+1);
+            passCount++;
+        }
+        else {
+            printf("Student %d failed the exam\n", i+1);
+            failCount++;
+        }
+    }
+
+    printf("The maximum marks obtained in the class is %d\n", maxMarks);
+    printf("%d students passed the exam and %d students failed the exam\n", passCount, failCount);
+    
   return 0;
 }
