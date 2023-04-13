c8a6cf544f4cc5325f8f8245d6def5ddb44281a5
BCA-Dristhi-7286 <127825872+BCA-Dristhi-7286@users.noreply.github.com>
2023-04-13 08:23:09 -0700

    Update Assignment6.c

--- Assignment6.c
+++ Assignment6.c
@@ -2,5 +2,22 @@
 
 int main()
 {
+  int countDistinct(int arr[], int n)
+{
+    int i, count = 1;
+    for (i = 1; i < n; i++) {
+        if (arr[i] != arr[i-1]) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    int arr[] = {1, 2, 2, 3, 4, 4, 4, 5, 5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    int distinctCount = countDistinct(arr, n);
+    printf("Number of distinct elements in the array is %d", distinctCount);
   return 0;
 }
