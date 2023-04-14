6b310fcb52a08f444a59d2672d00b434d62dac09
BCA-Dristhi-7286 <127825872+BCA-Dristhi-7286@users.noreply.github.com>
2023-04-14 12:17:59 +0530

    Update Assignment9.c

--- Assignment9.c
+++ Assignment9.c
@@ -5,8 +5,29 @@
 //.       m = 1
 //.       n = 1
 
-int main()
-{
-  
-  return 0;
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[100];
+    int i, j, l, count;
+    
+    printf("Enter a name: ");
+    scanf("%s", name);
+    
+    l = strlen(name);
+    
+    for(i=0; i<l; i++) {
+        count = 1;
+        if(name[i] == '\0') continue;
+        for(j=i+1; j<l; j++) {
+            if(name[i] == name[j]) {
+                count++;
+                name[j] = '\0';
+            }
+        }
+        printf("%c - %d\n", name[i], count);
+    }
+    
+    return 0;
 }
