ff9f5b868d09b42e4107f0718e935b39be823603
BCA-Dristhi-7286 <127825872+BCA-Dristhi-7286@users.noreply.github.com>
2023-04-14 14:40:56 +0530

    Update Assignment7.c

--- Assignment7.c
+++ Assignment7.c
@@ -1,6 +1,33 @@
 //Write a code find the length of a string and its reverse equivalent without using readymade c functions
 
-int main()
-{
-  return 0;
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int len = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // Find length of the string
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    printf("Length of string: %d\n", len);
+
+    // Reverse the string
+    int i, j;
+    char temp;
+
+    for (i = 0, j = len - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+
+    printf("Reverse string: %s\n", str);
+
+    return 0;
 }
+
