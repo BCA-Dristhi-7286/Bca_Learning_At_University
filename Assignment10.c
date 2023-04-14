a2c1fe0867c5fb8cdfbb0744c0310cb812b39b18
BCA-Dristhi-7286 <127825872+BCA-Dristhi-7286@users.noreply.github.com>
2023-04-14 14:43:25 +0530

    Update Assignment10.c

--- Assignment10.c
+++ Assignment10.c
@@ -5,10 +5,32 @@
 
 
 int findType(int n, int *) //complete this function
-{
-  
-}
-int main()
-{
-  return 0;
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int len = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    // find length of string
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    printf("Length of string: %d\n", len);
+
+    // reverse string
+    int i, j;
+    char temp;
+    for (i = 0, j = len - 1; i < j; i++, j--) {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+    }
+
+    printf("Reverse string: %s\n", str);
+
+    return 0;
 }
