d5bf5a990aa2b2432da8cd8c2ac22618afa9a6d5
BCA-Dristhi-7286 <127825872+BCA-Dristhi-7286@users.noreply.github.com>
2023-04-14 14:45:48 +0530

    Update Assignment11.c

--- Assignment11.c
+++ Assignment11.c
@@ -1,8 +1,21 @@
 //Convert an decimal to its binary equivalent using a bitwise operator
 
-int main()
-{
-  int N = 7;
-  
-  return 0;
+#include <stdio.h>
+
+int main() {
+    int N = 7;
+    int binary = 0;
+    int bit_position = 1;
+    
+    while (N > 0) {
+        int bit = N % 2;
+        binary += bit * bit_position;
+        bit_position *= 10;
+        N /= 2;
+    }
+    
+    printf("Decimal %d in binary is %d\n", N, binary);
+    
+    return 0;
 }
+
