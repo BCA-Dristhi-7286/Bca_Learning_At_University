cc5ff460a3c13e1b7a12d4cab9052f08e3a79e91
BCA-Dristhi-7286 <127825872+BCA-Dristhi-7286@users.noreply.github.com>
2023-04-14 12:21:15 +0530

    Update Assignment8.c

--- Assignment8.c
+++ Assignment8.c
@@ -2,7 +2,33 @@
 //input: Anitha
 //output: Aahint 
 
-int main()
-{
-  return 0;
+#include <stdio.h>
+#include <string.h>
+#define MAX_STRING_LENGTH 100
+
+int main() {
+    char str[MAX_STRING_LENGTH];
+    int i, j, len;
+    char temp;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    len = strlen(str);
+
+    // bubble sort algorithm
+    for (i = 0; i < len-1; i++) {
+        for (j = 0; j < len-i-1; j++) {
+            if (str[j] > str[j+1]) {
+                temp = str[j];
+                str[j] = str[j+1];
+                str[j+1] = temp;
+            }
+        }
+    }
+
+    printf("Sorted string: %s\n", str);
+
+    return 0;
 }
+
